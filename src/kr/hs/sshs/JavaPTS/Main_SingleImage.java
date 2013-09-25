53ef8f15a3c26c803eeeaea6bdaf8ba7914e6887
Stephen Kim <stephen422@gmail.com>
2013-09-25 21:53:57 +0900

    Now has (almost)complete functionality

--- src/kr/hs/sshs/JavaPTS/Main_SingleImage.java
+++ src/kr/hs/sshs/JavaPTS/Main_SingleImage.java
@@ -1,5 +1,9 @@
 package kr.hs.sshs.JavaPTS;
 
+import static com.googlecode.javacv.cpp.opencv_core.*;
+import static com.googlecode.javacv.cpp.opencv_imgproc.*;
+import static com.googlecode.javacv.cpp.opencv_highgui.*;
+
 import com.googlecode.javacv.CanvasFrame;
 import com.googlecode.javacv.FrameGrabber;
 import com.googlecode.javacv.FrameRecorder;
@@ -7,7 +11,7 @@ import com.googlecode.javacv.cpp.opencv_core.IplImage;
 
 public class Main_SingleImage {
 	// / Path to store resources
-	private static String PATH = "/d/";
+	static final String PATH = "D:\\video\\";
 
 	// / OpenCV Canvases
 	static CanvasFrame canvas1; // Canvas for showing result image
@@ -21,4 +25,19 @@ public class Main_SingleImage {
 	IplImage imgOrig;		// Original image (RGB)
 	IplImage imgBW; 		// Original blackwhite image
 	IplImage imgResult; 	// Result image
+	
+	
+	public static void main(String[] args) throws InterruptedException {
+		Main_SingleImage m = new Main_SingleImage();
+		m.imgOrig = cvLoadImage(PATH+"137.jpg");
+		
+		canvas1 = new CanvasFrame("original", CV_WINDOW_AUTOSIZE);
+		canvas2 = new CanvasFrame("result", CV_WINDOW_AUTOSIZE);
+		
+		canvas1.showImage(m.imgOrig);
+		canvas1.waitKey(0);
+		
+		m.imgOrig.release();
+		canvas1.dispose();
+	}	
 }
